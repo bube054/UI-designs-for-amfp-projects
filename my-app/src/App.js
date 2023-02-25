4e04686d33d264c15034d276c65ab9d979e11d0f
bube <attahgbube@gmail.com>
2023-02-25 06:33:34 +0100

    almost done with right hand side of view job design

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,8 @@
-import { IconFile, IconMapPinFilled } from '@tabler/icons-react';
+import React from 'react';
+import "./leaflet.css"
+import { IconFile, IconLollipop, IconMapPinFilled, IconPhone, IconMail} from '@tabler/icons-react';
+import { MapContainer, TileLayer, useMap , Marker, Popup} from 'react-leaflet'
+const position = [51.505, -0.09]
 
 function App() {
   return (
@@ -144,7 +148,7 @@ function App() {
           </div>
           <hr className="my-4 bg-black border border-black"/>
         </div>
-        <div className="basis-[20%]">
+        <div className="basis-[20%] space-y-1.5">
           <div className='space-y-2 border-2 border-main rounded-lg py-2'>
             <div className='flex items-center space-x-2 justify-start px-2'>
               <h5 className='text-xl font-bold capitalize '>Pay</h5>
@@ -170,6 +174,79 @@ function App() {
                 <span className=''>8 Max</span>
               </div>
             </div>
+          </div>
+          <div className='space-y-2 border-2 border-main rounded-lg py-2'>
+            <h5 className='text-xl font-bold capitalize px-2'>Location</h5>
+            <div className='px-2 text-sm'>
+              <p className='text-base'>New York</p>
+              <p className=''>New York</p>
+              <p className=''>New York, NY 10018 US</p>
+              <p className=''>Commercial</p>
+            </div>
+            <div className='w-full h-52'>
+              <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={position}>
+                  <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                  </Popup>
+                </Marker>
+              </MapContainer>
+            </div>
+          </div>
+          <div className='space-y-2 border-2 border-main rounded-lg py-2'>
+            <div className='p-2'>
+              <h5 className="text-xl font-bold capitalize">Assigned To</h5>
+              <div className={`flex items-center space-x-4`}>
+                <div className={`flex-shrink-0`}>
+                  <div className={`inline-flex overflow-hidden relative justify-center items-center w-10 h-10 border border-secondary bg-gray-100 rounded-full uppercase`}>
+                    {/* <span className={`font-medium text-gray-600`}>{user.first_name.slice(0,1)}{user.last_name.slice(0,1)}</span> */}
+                    <span className={`font-medium text-gray-600`}>DF</span>
+                  </div>
+                  {/* <img class="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" alt="Neil"/> */}
+                </div>
+                <div className={`flex-1 min-w-0`}>
+                  <p className={`text-base font-semibold text-black truncate capitalize`}>
+                    Deji Faruq
+                    {/* {user.first_name} {user.last_name} */}
+                  </p>
+                  <p className={`text-sm text-ash truncate`}>08099778654</p>
+                  {/* <p className={`text-sm text-ash truncate`}>{user.phone || "————————"}</p> */}
+                </div>
+              </div>
+            </div>
+            <hr className="h-[1px] bg-main border border-main"/>
+            <div className='p-2'>
+              <h5 className="text-xl font-bold capitalize">Contact Person</h5>
+              <div className={`flex items-center space-x-4`}>
+                <div className={`flex-shrink-0`}>
+                  <div className={`inline-flex overflow-hidden relative justify-center items-center w-10 h-10 border border-secondary bg-gray-100 rounded-full uppercase`}>
+                    {/* <span className={`font-medium text-gray-600`}>{user.first_name.slice(0,1)}{user.last_name.slice(0,1)}</span> */}
+                    <span className={`font-medium text-gray-600`}>DF</span>
+                  </div>
+                  {/* <img class="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" alt="Neil"/> */}
+                </div>
+                <div className={`flex-1 min-w-0`}>
+                  <p className={`text-base font-semibold text-black truncate capitalize`}>
+                    Deji Faruq
+                    {/* {user.first_name} {user.last_name} */}
+                  </p>
+                  <p className={`text-sm text-ash truncate`}>08099778654</p>
+                  {/* <p className={`text-sm text-ash truncate`}>{user.phone || "————————"}</p> */}
+                </div>
+              </div>
+              <div className='text-xs font-semibold'>
+                <div className='flex items-center justify-start space-x-1'> <IconPhone className='w-5 h-5 text-main inline'/> <span className=''>090875678</span> </div>
+                <div className='flex items-center justify-start space-x-1'> <IconPhone className='w-5 h-5 text-main inline'/> <span className=''>attahgbube@gmail.com</span> </div>
+              </div>
+            </div>
+          </div>
+          <div className='flex items-center justify-between px-2'>
+            <button className='focus:outline-none text-white bg-red-600 hover:bg-red-700 font-semibold text-xs sm:text-sm md:text-base px-2 py-2'>Cancel Job</button>
+            <button className='focus:outline-none text-white bg-red-700 hover:bg-red-800 font-semibold text-xs sm:text-sm md:text-base px-2 py-2'>File A Complaint</button>
           </div>
         </div>
       </div>
